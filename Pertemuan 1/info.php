41ce1f0c73c43cad85674f244324b524b76fc315
MuhammadIkhsanNoorFadhillah <146808218+MuhammadIkhsanNoorFadhillah@users.noreply.github.com>
2024-03-06 09:18:08 +0700

    Add files via upload

---
+++ Pertemuan 1/info.php
@@ -0,0 +1,11 @@
+<?php
+// definisikan variables
+$nama = 'Muhammad Ikhsan Noor Fadhillah';
+$umur = 18;
+$berat = 52;
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+echo "<br/>Hai Apa Kabar Muhammad Ikhsan Noor Fadhillah";
+echo " <br/>Apakah Kamu Udah Siap Untuk Jalan?";
+?>
---
+++ Pertemuan 1/variabel_user.php
@@ -0,0 +1,8 @@
+<?php
+$nama = "Muhammad Ikhsan Noor Fadhillah";
+$umur = 18;
+$berat = 52;
+
+echo "nama : $nama";
+echo "<br/>umur : $umur tahun";
+echo "<br/>berat : $berat kg";
